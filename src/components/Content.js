b5d2ca687b3c90a8de06d01e9c3615c5b0f3ac13
shreya <i.bakshi.shreya@gmail.com>
2025-03-11 23:47:50 +0000

    feat pass anonymous func to onClick handlers in button tags

--- src/components/Content.js
+++ src/components/Content.js
@@ -12,10 +12,28 @@ const Content = () => {
         alert('You clicked the button!');
     }
 
+    const handleClick2 = (name) => {
+        alert(`${name} clicked the button!`);
+    }
+
   return (
     <main className="content">
+        {/* call the handleNameChange once, only when page loads */}
+        {/* therefore, () after function name */}
         <p>Hello {handleNameChange()}!</p>
+
+        {/* pass the ref to the handleClick func ie no () after func name */}
+        {/* func will be called whenever button is clicked */}
+        {/* if  onClick={handleClick()} is present the func will be called immediately */}
         <button onClick={handleClick}>Click me!</button>
+
+        {/* Anonymous func are not called until button is clicked so parameters can be passed */}
+        {/* Anon func can be multiline so we need =>{} to write func body */}
+        {/* Anonymous func with parameter passed in onClick  */}
+        <button onClick={() => {handleClick2('Shreya')}}>Click me 2!</button>
+
+        {/* Anonymous func with function parameter passed in onClick  */}
+        <button onClick={() => {handleClick2(handleNameChange())}}>Click me 3!</button>
     </main>
   )
 }
